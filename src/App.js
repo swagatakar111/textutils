61f5b3f46c6e8ad19a44e1b50e67fedb7d67e9a1
swagatakar111 <144600656+swagatakar111@users.noreply.github.com>
2023-12-21 01:19:32 +0530

    1st change

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 
 import './App.css';
 import Alert from './components/Alert';
-import About from './components/About';
+// import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import React,{useState} from 'react';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  
-} from "react-router-dom";
+// import {
+//   BrowserRouter as Router,
+//   Routes,
+//    Route
+//  } from "react-router-dom";
 
 function App() {
   const [mode,setMode]= useState('light');
@@ -46,19 +45,22 @@ function App() {
   }
   return (
   <>
-      <Router>
-      {/* <Navbar title="textutils2" aboutText="aboutTextutils" /> */}
+     {/* <Router>  */}
+        
       <Navbar title="Textutils" mode={mode} toggleMode={toggleMode}/>
       <Alert alert={alert}/>
       <div className="container my-3">
-      <Routes>
+       {/* <Routes>
             <Route exact path="/about" element={<About />}>
+            </Route> 
+             <Route exact path="/" element={<TextForm heading="Enter text to analyze"  mode={mode} showAlert={showAlert} />}>
             </Route>
-            <Route exact path="/" element={<TextForm heading="Enter text to analyze"  mode={mode} showAlert={showAlert} />}>
-            </Route>
-          </Routes>
+          
+          </Routes>  */}
+          <TextForm showAlert={showAlert} heading="Enter the text to analyse below" mode={mode}/>
+          
         </div>
-      </Router>
+       {/* </Router>  */}
   
   </>
   );
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -40,20 +40,18 @@ export default function TextForm(props) {
     
         <textarea className="form-control"  value={text} onChange ={handleOnChange} style={{backgroundColor: props.mode==='dark'?'grey':'white',color: props.mode==='dark'?'white':'black'}} id="myBox" rows="8"></textarea>
         </div>
-        <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to UpperCase</button>
-        <button className="btn btn-primary mx-1" onClick={handleLoClick}>Convert to Lowerase</button>
-        <button className="btn btn-primary mx-1" onClick={handleblank}>Clear</button>
-        <button className="btn btn-primary mx-1" onClick={handleExtraSpaces}>Clear Extra space</button>
+        <button className="btn btn-primary mx-1 my-1" onClick={handleUpClick}>Convert to UpperCase</button>
+        <button className="btn btn-primary mx-1 my-1" onClick={handleLoClick}>Convert to Lowerase</button>
+        <button className="btn btn-primary mx-1 my-1" onClick={handleblank}>Clear</button>
+        <button className="btn btn-primary mx-1 my-1" onClick={handleExtraSpaces}>Clear Extra space</button>
     </div>
-    <div className="container my-3"  style={{color: props.mode==='dark'?'white':'black'}}>
-      <h1>Your text summary</h1>
-      <p>{text.split(" ").length}words and {text.length} characters</p>
-      <p>{0.008  * text.split(" ").length} Minutes read</p>
-      <h2>Preview</h2>
-      <p>{text.length>0?text:"Enter text above to preview here"}</p>
-
-
-    </div>
+    <div className="container my-3" style={{color: props.mode==='dark'?'white':'#042743'}}>
+            <h2>Your text summary</h2>
+            <p>{text.split(/\s+/).filter((element)=>{return element.length!==0}).length} words and {text.length} characters</p>
+            <p>{0.008 *  text.split(/\s+/).filter((element)=>{return element.length!==0}).length} Minutes read</p>
+            <h2>Preview</h2>
+            <p>{text.length>0?text:"Nothing to preview!"}</p>
+        </div>
    </>
    
       )
